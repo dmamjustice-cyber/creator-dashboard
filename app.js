221efc94bf9e23e373e604d799b267c779e8cfc2
dmamjustice-cyber <dmamjustice@gmail.com>
2025-12-05 15:02:00 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+function showSubscription() {
+  document.querySelector(".main").classList.add("hidden");
+  document.getElementById("subscriptionPage").classList.remove("hidden");
+}
+
+function showDashboard() {
+  document.querySelector(".main").classList.remove("hidden");
+  document.getElementById("subscriptionPage").classList.add("hidden");
+}
+
 console.log("Dashboard loaded.");
+
 
